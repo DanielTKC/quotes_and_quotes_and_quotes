60006d2aca979f1e76fb7c6db690c3ebd357373b
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2026-03-13 17:57:50 -0500

    gitignore

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php
     declare(strict_types = 1);
+    require_once('vendor/autoload.php');
+
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
 
     echo '<pre>';
     echo '<br>';
+    echo $_ENV['NAME'] . "\n";
     print_r($_SERVER);
     echo '</pre>';
 
