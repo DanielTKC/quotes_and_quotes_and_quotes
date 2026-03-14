5c7a6bc4fd84b995047c9162307a75c92e6cf401
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2026-03-14 18:49:40 -0500

    Quotes read api created

---
+++ api/quote/read.php
@@ -0,0 +1,47 @@
+<?php
+    // Headers
+    use models\Quote;
+
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json");
+    include_once '../../config/Database.php';
+    include_once '../../models/Quote.php';
+
+    // DB OBJECT & CONNECT
+    $database = new Database();
+    $db = $database->connect();
+
+    // Instantiate quote object
+    $quote = new Quote($db);
+
+    // Quote query
+    $result = $quote->read();
+    $num = $result->rowCount();
+
+    // Check if any quotes
+    if ($num > 0) {
+        $quotes_arr = array();
+        $quotes_arr['data'] = array();
+
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+            $quote_item = array(
+                'id' => $id,
+                'quote' => $quote,
+                'author_id' => $author_id,
+                'author_name' => $author_name,
+                'category_id' => $category_id,
+                'category_name' => $category_name,
+            );
+
+            // push to data
+            array_push($quotes_arr['data'], $quote_item);
+        }
+
+        // Turn to json
+        echo json_encode($quotes_arr);
+    } else {
+        echo json_encode(
+            array("message" => "No Quotes found.")
+        );
+    }
