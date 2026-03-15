5952e9d95a59b184fb99fe08e79b96e725ebc7ea
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2026-03-15 10:37:20 -0500

    index.php for authors

---
+++ api/authors/read.php
@@ -0,0 +1,8 @@
+<?php
+    use models\Author;
+    require_once '../../config/bootstrap.php';
+    require_once '../../models/Author.php';
+
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json");
+
