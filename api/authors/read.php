76fb1c1cc688930857f8b4ba1e3da0efc2210cab
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2026-03-15 10:44:46 -0500

    Authors read

--- api/authors/read.php
+++ api/authors/read.php
@@ -17,3 +17,23 @@
     $result = $author->read();
     $num = $result->rowCount();
 
+    if ($num > 0) {
+        $authors_array = array();
+
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+            $author_item = array(
+                'id' => $id,
+                'author' => $author,
+            );
+
+            $authors_arr[] = $author_item;
+        }
+
+        echo json_encode($authors_arr);
+    } else {
+        echo json_encode(
+            array("message" => "No Quotes found.")
+        );
+    }
+
