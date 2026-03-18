10b46cf364433eb7708b7d62410cf1e6b854841f
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2026-03-18 12:55:46 -0500

    delete added to indexphp

--- api/authors/index.php
+++ api/authors/index.php
@@ -36,6 +36,9 @@
         case 'PUT':
             require 'update.php';
             break;
+        case 'DELETE':
+            require 'delete.php';
+            break;
         default:
             echo json_encode(['message' => 'Method Not Allowed']);
             break;
