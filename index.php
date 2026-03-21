52e561a75ac1d3fe3bb958d0f2c42efbba272d39
Daniel Terreros <110783387+DanielTKC@users.noreply.github.com>
2026-03-21 10:26:51 -0500

    Simplified landing page

--- index.php
+++ index.php
@@ -1,15 +1,2 @@
 <?php
-    require_once('config/bootstrap.php');
-
-    $db = new Database();
-    $conn = $db->connect();
-
-    if ($conn) {
-        $stmt = $conn->prepare('SELECT * FROM quotes WHERE id = :id');
-        $stmt->execute(['id' => 1]);
-        $quote = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        echo '<pre>';
-        print_r($quote);
-        echo '</pre>';
-    }
+    echo '<pre>Directory of api is in /api/</pre>';
